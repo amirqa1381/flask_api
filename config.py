abde1dd83494b66f8b5fc4857c51c1fa048f527f
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-05-18 09:40:06 +0330

    This file is for configuration of the app and sqlAlchemy

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# config.py
+
 import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
@@ -7,11 +9,8 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-
-
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
